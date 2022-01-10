f35ea10dd47743e35d1e74daef1e29024cad12af
Sunanda123-png <sunanda02@gmail.com>
2022-01-10 13:53:28 +0530

    Add: User loging and checking user exist in databse or not

--- fundooNotes/user/urls.py
+++ fundooNotes/user/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = {
     path('user_registration', views.user_registration, name='user_registration'),
+    path('user_login',views.user_login, name='user_login'),
 }
--- fundooNotes/user/views.py
+++ fundooNotes/user/views.py
@@ -2,15 +2,17 @@ import json
 import logging
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-
 from .models import User
+
+logging.basicConfig(filename="views.log", filemode="w")
+
 
 @csrf_exempt
 def user_registration(request):
     """
     this method is created for user registration
-    :param request: passing the request
-    :return: outcome result
+    :param request: web request for user details sotoring
+    :return: response
     """
     try:
         if request.method == "POST":
@@ -26,5 +28,24 @@ def user_registration(request):
                         password=password, email=email)
             user.save()
             return HttpResponse("Data stored successfully")
+    except Exception as e:
+        logging.error(e)
+
+
+@csrf_exempt
+def user_login(request):
+    """
+    this method is created for user_login
+    :param request: web request for login
+    :return:
+    """
+    try:
+        if request.method == "POST":
+            data = json.loads(request.body)
+            user_name = data["user_name"]
+            password = data["password"]
+
+            if User.objects.filter(user_name=user_name, password=password):
+                return HttpResponse("User is exist")
     except Exception as e:
         logging.error(e)
