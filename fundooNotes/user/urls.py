a465ea77b1892c0a408361c93667e8313acc9a28
Sunanda123-png <sunanda02@gmail.com>
2022-01-16 09:12:49 +0530

    Add:Serialization of data

--- fundooNotes/user/urls.py
+++ fundooNotes/user/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = {
-    path('user_registration', views.user_registration, name='user_registration'),
-    path('user_login',views.user_login, name='user_login'),
+    path('user_registration', views.UserRegistration.as_view(), name='user_registration'),
 }
--- fundooNotes/user/views.py
+++ fundooNotes/user/views.py
@@ -1,60 +1,44 @@
-import json
 import logging
 
-from django.contrib.auth.models import auth
-from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .models import User
 
+from .models import User
+from .serializers import UserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 logging.basicConfig(filename="views.log", filemode="w")
 
 
-@csrf_exempt
-def user_registration(request):
+class UserRegistration(APIView):
     """
-    this method is created for user registration
-    :param request: web request for user details storing
-    :return: response
+    class based views for User registration
     """
-    try:
-        if request.method == "POST":
-            data = json.loads(request.body)
-            user_name = data.get("user_name")
-            first_name = data.get("first_name")
-            last_name = data.get("last_name")
-            age = data.get("age")
-            password = data.get("password")
-            email = data.get("email")
+    def get(self, request):
+        """
+        this method is created for retrieve data
+        :param request: format of the request
+        :return: Response
+        """
+        try:
+            user = User.objects.all()
+            serializer = UserSerializer(user)
+            return Response(serializer.data)
+        except Exception as e:
+            logging.error(e)
 
-            if User.objects.filter(username=user_name).exists():
-                return JsonResponse({"message":"This user name is already taken"})
-            elif User.objects.filter(email=email).exists():
-                return JsonResponse({"message":"This email is already taken"})
-            else:
-                user = User.objects.create_user(username=user_name, first_name=first_name, last_name=last_name, age=age,password=password, email=email, is_verified=0)
-                user.save()
-                return JsonResponse({"message":"data added successfully","data":data})
-    except Exception as e:
-        logging.error(e)
-        print(e)
-
-
-@csrf_exempt
-def user_login(request):
-    """
-    this method is created for user_login
-    :param request: web request for login
-    :return:
-    """
-    try:
-        if request.method == "POST":
-            data = json.loads(request.body)
-            print(json.loads(request.body))
-            user=auth.authenticate(username=data.get("user_name"), password=data.get("password"))
-            if user is not None:
-                return JsonResponse({"message":"User is valid", "data":data})
-            else:
-                return JsonResponse({"message": "User is invalid"})
-    except Exception as e:
-        logging.error(e)
+    def post(self, request):
+        """
+        this method is created for inserting the data
+        :param request: format of the request
+        :return: Response
+        """
+        try:
+            serializer = UserSerializer(data=request.data)
+            if serializer.is_valid():
+                User.objects.create_user()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            logging.error(e)
